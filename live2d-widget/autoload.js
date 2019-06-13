d4f6bbed746c08de19f652325c8acee8b57c01fd
yangshangqi <2685764101@qq.com>
2019-06-13 21:13:17 +0800

    Site updated: 2019-06-13 21:13:09

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -3,25 +3,25 @@
 const live2d_path = "/live2d-widget/";
 
 //加载waifu.css
-$("<link>").attr({href: live2d_path + "waifu.css", rel: "stylesheet", type: "text/css"}).appendTo("head");
+$("<link>").attr({href: "/live2d_path/waifu.css", rel: "stylesheet", type: "text/css"}).appendTo("head");
 
 //加载live2d.min.js
 $.ajax({
-	url: live2d_path + "live2d.min.js",
+	url: "/live2d_path/live2d.min.js",
 	dataType: "script",
 	cache: true
 });
 
 //加载waifu-tips.js
 $.ajax({
-	url: live2d_path + "waifu-tips.js",
+	url: "/live2d_path/waifu-tips.js",
 	dataType: "script",
 	cache: true
 });
 
 //初始化看板娘，会自动加载指定目录下的waifu-tips.json
 $(window).on("load", function() {
-	initWidget(live2d_path + "waifu-tips.json", "https://live2d.fghrsh.net/api");
+	initWidget("/live2d_path/waifu-tips.json", "https://live2d.fghrsh.net/api");
 });
 //initWidget第一个参数为waifu-tips.json的路径
 //第二个参数为api地址（无需修改）
